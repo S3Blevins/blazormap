1846dfb57e91c71fcc5566f2662f5ec0c916e412
sterling.blevins <s3.blevins@gmail.com>
2025-05-17 21:53:00 -0700

    Added leaflet map and added mouse event handler to update position.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # blazormap
 
 * Uses map
-* Draws on map
 * Positional indicator
-* Requests data from map
 * Menu bar
 * Modal windows
 
--- wwwroot/JsWrappers/LeafletWrapper.js
+++ wwwroot/JsWrappers/LeafletWrapper.js
@@ -1,1 +1,11 @@
-﻿
+﻿let map = L.map('map', { cursor: true }).setView({ lon: -118.26544, lat: 34.23555 }, 16);
+let pos;
+
+export function load_map() {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 9 }).addTo(map);
+    map.on('mousemove', function (e) { pos = e.latlng.toString() });
+}
+
+export function on_move() {
+    return pos;
+}
